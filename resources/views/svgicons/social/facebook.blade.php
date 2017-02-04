a58f31c4e8bdb81f8f1f64f51b7ab7ebdc799aae
Michael Joyner <joyner.michael@gmail.com>
2017-02-04 16:41:21 +0800

    update social svg icons

--- resources/views/svgicons/social/facebook.blade.php
+++ resources/views/svgicons/social/facebook.blade.php
@@ -1,6 +1,7 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52.32 52.32"  height="50" width="50">
-    <path class="colour-in" fill="#803EA0"
-          d="M21.42 21.84h2.64v-2.56c0-1.13 0-2.87.85-4A4.69 4.69 0 0 1 29 13.41a16.57 16.57 0 0 1 4.73.48l-.66 3.91a8.9 8.9 0 0 0-2.12-.32c-1 0-1.94.37-1.94 1.39v3h4.21l-.29 3.82H29v13.23h-4.94V25.66h-2.64z"/>
-    {{--<rect class="icon-tile" fill="none" stroke="#ffffff" stroke-miterlimit="10" stroke-width="3" x="1.5" y="1.5" width="49.32"--}}
-          {{--height="49.32" rx="8.69" ry="8.69"/>--}}
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54.02 54.02">
+    <title>facebook</title>
+    <path class="colour-stroke" fill="none" stroke="#2e2e34" stroke-miterlimit="10" stroke-width="3px"
+          d="M27,52.52A25.51,25.51,0,1,0,1.5,27,25.51,25.51,0,0,0,27,52.52"/>
+    <path fill="#2e2e34" class="colour-fill"
+          d="M20.86,22.69h2.64V20.13c0-1.13,0-2.87.85-4a4.69,4.69,0,0,1,4.09-1.92,16.57,16.57,0,0,1,4.73.48l-.66,3.91a8.9,8.9,0,0,0-2.12-.32c-1,0-1.94.37-1.94,1.39v3h4.21l-.29,3.82H28.44V39.77H23.49V26.51H20.86Z"/>
 </svg>
