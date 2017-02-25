5a5a043d04bc2d0a5bda14ac12d4abff3271b03a
Michael Joyner <joyner.michael@gmail.com>
2017-02-25 11:22:56 +0800

    close mobile menu when hash link for about page clicked

--- resources/assets/js/components/Menuassistant.js
+++ resources/assets/js/components/Menuassistant.js
@@ -2,7 +2,9 @@ export default {
 
     init() {
         const trigger = document.querySelector('#nav-trigger');
+        const hashLinks = Array.prototype.slice.call(document.querySelectorAll('.about-nav a'));
         trigger.addEventListener('change', (ev) => this.toggleMenuStates(ev), false);
+        hashLinks.forEach(link => link.addEventListener('click', () => this.closeMenu(), false));
     },
 
     toggleMenuStates(ev) {
@@ -11,5 +13,9 @@ export default {
         } else {
             return document.body.classList.remove('overlaid');
         }
+    },
+
+    closeMenu() {
+        document.querySelector('#nav-trigger').checked = false;
     }
 }
