122b6b6b496a7475598d1d9102c0b5ffc94ed6e9
Michael Joyner <joyner.michael@gmail.com>
2017-02-25 11:15:16 +0800

    order main nav items

--- resources/views/front/partials/navbar.blade.php
+++ resources/views/front/partials/navbar.blade.php
@@ -7,7 +7,33 @@
     <div class="menu-container">
         <ul class="main-nav-list">
             <label for="nav-trigger" class="nav-trigger-close-icon">&times;</label>
-            <li class="nav-item @activenav('about')">
+            <li class="articles-nav nav-item @activenav('articles')">
+                <a href="{{ localUrl('/articles') }}">{{ trans('navbar.articles') }}</a>
+                <span class="nav-caret"> @include('svgicons.downcaret')</span>
+                <ul class="secondary-nav-list">
+                    @foreach($navCategories as $navCat)
+                        <li class="secondary-nav-item">
+                            <a href="{{ localUrl('/categories/' . $navCat->slug) }}">{{ $navCat->getTranslation('name', Localization::getCurrentLocale()) }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </li>
+            <li class="gallery-nav nav-item @activenav('galleries')">
+                <a href="{{ localUrl('/galleries') }}">{{ trans('navbar.gallery') }}</a>
+                <span class="nav-caret"> @include('svgicons.downcaret')</span>
+                <ul class="secondary-nav-list">
+                    <li class="secondary-nav-item">
+                        <a href="{{ localUrl('/galleries/photos') }}">{{ trans('navbar.photos') }}</a>
+                    </li>
+                    <li class="secondary-nav-item">
+                        <a href="{{ localUrl('/galleries/art') }}">{{ trans('navbar.art') }}</a>
+                    </li>
+                    <li class="secondary-nav-item">
+                        <a href="{{ localUrl('/galleries/videos') }}">{{ trans('navbar.video') }}</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="about-nav nav-item @activenav('about')">
                 <a href="{{ localUrl('/about') }}">{{ trans('navbar.about') }}</a>
                 <span class="nav-caret"> @include('svgicons.downcaret')</span>
                 <ul class="secondary-nav-list">
@@ -28,36 +54,10 @@
                     </li>
                 </ul>
             </li>
-            <li class="nav-item @activenav('articles')">
-                <a href="{{ localUrl('/articles') }}">{{ trans('navbar.articles') }}</a>
-                <span class="nav-caret"> @include('svgicons.downcaret')</span>
-                <ul class="secondary-nav-list">
-                    @foreach($navCategories as $navCat)
-                        <li class="secondary-nav-item">
-                            <a href="{{ localUrl('/categories/' . $navCat->slug) }}">{{ $navCat->getTranslation('name', Localization::getCurrentLocale()) }}</a>
-                        </li>
-                    @endforeach
-                </ul>
-            </li>
-            <li class="nav-item @activenav('galleries')">
-                <a href="{{ localUrl('/galleries') }}">{{ trans('navbar.gallery') }}</a>
-                <span class="nav-caret"> @include('svgicons.downcaret')</span>
-                <ul class="secondary-nav-list">
-                    <li class="secondary-nav-item">
-                        <a href="{{ localUrl('/galleries/photos') }}">{{ trans('navbar.photos') }}</a>
-                    </li>
-                    <li class="secondary-nav-item">
-                        <a href="{{ localUrl('/galleries/art') }}">{{ trans('navbar.art') }}</a>
-                    </li>
-                    <li class="secondary-nav-item">
-                        <a href="{{ localUrl('/galleries/videos') }}">{{ trans('navbar.video') }}</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="nav-item @activenav('bios')">
+            <li class="bios-nav nav-item @activenav('bios')">
                 <a href="{{ localUrl('/bios') }}">{{ trans('navbar.bios') }}</a>
             </li>
-            <li class="nav-item">
+            <li class="affiliates-nav nav-item">
                 <a href="{{ localUrl('/affiliates') }}">{{ trans('navbar.affiliates') }}</a>
             </li>
             @foreach(Localization::getSupportedLocales() as $localeCode => $properties)
