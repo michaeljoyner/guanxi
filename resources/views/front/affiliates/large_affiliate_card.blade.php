e34352a93986b2dceb118d4eb4391b6bb2a0b018
Michael Joyner <joyner.michael@gmail.com>
2017-06-27 08:54:20 +0800

    fix affiliate index card image aspect ratio

--- resources/views/front/affiliates/large_affiliate_card.blade.php
+++ resources/views/front/affiliates/large_affiliate_card.blade.php
@@ -1,9 +1,11 @@
 <div class="large-affiliate-card two-piece">
     <p class="affiliate-name purple-text heavy-heading">{{ $affiliate->name }}</p>
     <a href="{{ localUrl('/affiliates/' . $affiliate->slug) }}">
-        <div class="card-image-holder">
-            <img src="{{ $affiliate->imageSrc('web') }}" width="500" height="400" alt="">
-            <p class="hover-action-indicator">{{ trans('homepage.affiliates.hover_text') }}</p>
+        <div class="card-image-outer-holder">
+            <div class="card-image-holder">
+                <img src="{{ $affiliate->imageSrc('web') }}" alt="">
+                <p class="hover-action-indicator">{{ trans('homepage.affiliates.hover_text') }}</p>
+            </div>
         </div>
     </a>
     <p class="affiliate-address light-heading">{{ $affiliate->location }}</p>
