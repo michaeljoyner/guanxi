584ae1b60a1567247890a2ed4274fe229347b444
Michael Joyner <joyner.michael@gmail.com>
2017-06-30 09:46:23 +0800

    change banner text colour and fix associate hover issue on hp

--- resources/views/front/home/affiliatecard.blade.php
+++ resources/views/front/home/affiliatecard.blade.php
@@ -1,7 +1,7 @@
 <div class="affiliate-card four-piece">
     <a href="{{ localUrl('/affiliates/' . $affiliate->slug) }}">
         <div class="card-image-holder">
-            <img src="{{ $affiliate->imageSrc('thumb') }}" width="150" height="150" alt="">
+            <img src="{{ $affiliate->imageSrc('thumb') }}" alt="">
             <p class="hover-action-indicator">{{ trans('homepage.affiliates.hover_text') }}</p>
         </div>
     </a>
