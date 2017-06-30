dc94601a39b45cd0d44edea36657c721d73f9759
Michael Joyner <joyner.michael@gmail.com>
2017-06-30 09:13:31 +0800

    article card hover image issue

--- resources/views/front/home/articlecard.blade.php
+++ resources/views/front/home/articlecard.blade.php
@@ -7,7 +7,7 @@
         @endif
     <a href="{{ localUrl('/articles/' . $article->slug) }}">
         <div class="card-image-holder">
-            <img src="{{ $article->titleImg('thumb') }}" width="250" height="200" alt="{{ $article->getTranslation('title', Localization::getCurrentLocale()) }}">
+            <img src="{{ $article->titleImg('thumb') }}" alt="{{ $article->getTranslation('title', Localization::getCurrentLocale()) }}">
             <p class="hover-action-indicator">{{ trans('homepage.articles.hover_text') }}</p>
         </div>
     </a>
