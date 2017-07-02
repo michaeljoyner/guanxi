5414710a5ba868bf1ec4aa19d3aff7f9a0895b7f
Michael Joyner <joyner.michael@gmail.com>
2017-07-02 11:18:32 +0800

    register sitemap command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\CacheWeather;
+use App\Console\Commands\GenerateSiteMap;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        CacheWeather::class
+        CacheWeather::class,
+        GenerateSiteMap::class
     ];
 
     /**
