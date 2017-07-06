b06e55d9301b42c60d58df4772919b84442ffef4
Michael Joyner <joyner.michael@gmail.com>
2017-07-06 08:30:53 +0800

    use media, not magazine

--- resources/lang/en/meta.php
+++ resources/lang/en/meta.php
@@ -2,31 +2,31 @@
 
 return [
     'home' => [
-        'title' => 'Guanxi Magazine',
-        'description' => 'Guanxi Magazine publishes unique and original content about culture, lifestyle, politics, Taiwan, and the world.'
+        'title' => 'Guanxi Media',
+        'description' => 'Guanxi Media publishes unique and original content about culture, lifestyle, politics, Taiwan, and the world.'
     ],
     'about' => [
-        'title' => 'All About Guanxi Magazine',
+        'title' => 'All About Guanxi Media',
         'description' => 'Discover our story, our mission, and our vision. Also, stay connected to what’s happening in Taiwan, and find out how to contact us.'
     ],
     'articles' => [
-        'title' => 'Articles | Guanxi Magazine',
+        'title' => 'Articles | Guanxi Media',
         'description' => 'Read articles on lifestyle, culture, and politics, in Taiwan and globally, written by a diverse mix of talented journalists and storytellers.'
     ],
     'gallery' => [
-        'title' => 'Gallery | Guanxi Magazine',
+        'title' => 'Gallery | Guanxi Media',
         'description' => 'View photography, artwork, and videos, from international creators.'
     ],
     'photos' => [
-        'title' => 'Photos | Galleries of Guanxi Magazine photography',
+        'title' => 'Photos | Galleries of Guanxi Media photography',
         'description' => 'Featured photography and photographers'
     ],
     'artworks' => [
-        'title' => 'Artwork | Guanxi Magazine loves art',
+        'title' => 'Artwork | Guanxi Media loves art',
         'description' => 'Featured artworks and artists'
     ],
     'videos' => [
-        'title' => 'Videos | Videos by Guanxi Magazine',
+        'title' => 'Videos | Videos by Guanxi Media',
         'description' => 'Entertaining videos about Taiwan'
     ],
     'bios' => [
@@ -34,7 +34,7 @@ return [
         'description' => 'Meet the fascinating people behind the content.'
     ],
     'affiliates' => [
-        'title' => 'Associates of Guanxi Magazine',
+        'title' => 'Associates of Guanxi Media',
         'description' => 'Here at GuanXi, relationships are important. Here are some of the local businesses and organizations we share mutual support with.'
     ]
 ];
