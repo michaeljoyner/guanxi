da2a4b50310b9c087e7e341927528cb30659df29
Michael Joyner <joyner.michael@gmail.com>
2018-03-20 10:09:12 +0800

    add favicon

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -10,6 +10,13 @@
     @yield('head')
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans|Noto+Serif|Oswald:300,700" rel="stylesheet">
     <meta name="google-site-verification" content="RJF0Jhe0QhWiDFeHN9rhO1UpEMG0RUXGATG7nfS8xFM" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="theme-color" content="#803EA0">
 </head>
 <body class="@yield('bodyclass', 'scripted')">
 <!--[if lt IE 8]>
