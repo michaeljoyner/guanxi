8fd6c0e3af0f39b2aab9591e9dad2423a9b2dbec
Michael Joyner <joyner.michael@gmail.com>
2020-04-07 12:41:22 +0800

    release testimonials

--- resources/views/front/home/testimonials.blade.php
+++ resources/views/front/home/testimonials.blade.php
@@ -1,10 +1,10 @@
-<section class="flex justify-between home-testimonials border-b-2 border-black">
-    <div class="w-2/5 bg-black flex justify-center items-center py-32">
-        @include('svg.logo_purple', ['classes' => 'h-40 banner-logo'])
+<section class="flex flex-col md:flex-row justify-between home-testimonials border-b-2 border-black">
+    <div class="w-full md:w-2/5 home-banner flex justify-center items-center py-32 px-4">
+        @include('svg.logo_media', ['classes' => 'h-40 banner-logo'])
     </div>
-    <div class="w-3/5 py-12 px-6">
+    <div class="w-full md:w-3/5 py-12 px-6">
         <span class="type-h5 border-b-2 border-brand-purple text-brand-purple">What people are saying</span>
-        <div class="my-12">
+        <div class="my-12 max-w-2xl mx-auto">
             @foreach($testimonials as $testimonial)
                 <x-testimonial-card :testimonial="$testimonial" :is-left="$loop->odd"></x-testimonial-card>
             @endforeach
