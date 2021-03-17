e2653455bf0540230910484fed81b22066ca1f6e
Michael Joyner <joyner.michael@gmail.com>
2021-03-17 09:34:04 +0800

    missing translations

--- resources/lang/zh/articles.php
+++ resources/lang/zh/articles.php
@@ -3,8 +3,8 @@
 return [
     'page' => [
         'title' => '文章',
-        'taiwan_title' => 'Taiwan Articles',
-        'world_title' => 'World Articles',
+        'taiwan_title' => '台灣',
+        'world_title' => '全球',
         'intro' => '作家及記者探索台灣與世界的多樣主題',
         'intro_taiwan' => '作家及記者探索台灣與世界的多樣主題',
         'intro_world' => '作家及記者探索台灣與世界的多樣主題'
