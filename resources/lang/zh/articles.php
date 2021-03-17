035043e7afb73a54407c88e49c93002a0b0231bb
Michael Joyner <joyner.michael@gmail.com>
2021-03-17 09:29:24 +0800

    translation fix

--- resources/lang/zh/articles.php
+++ resources/lang/zh/articles.php
@@ -6,7 +6,7 @@ return [
         'taiwan_title' => 'Taiwan Articles',
         'world_title' => 'World Articles',
         'intro' => '作家及記者探索台灣與世界的多樣主題',
-        'intro_taiwan' => 'Storytellers and journalists exploring a variety of topics in Taiwan.',
-        'intro_world' => 'Storytellers and journalists exploring a variety of topics worldwide.'
+        'intro_taiwan' => '作家及記者探索台灣與世界的多樣主題',
+        'intro_world' => '作家及記者探索台灣與世界的多樣主題'
     ]
 ];
