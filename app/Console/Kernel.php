956d43e00856a8cd2c2b752d35aa13e9a9f191cc
Michael Joyner <joyner.michael@gmail.com>
2021-05-09 09:21:26 +0800

    less weather checks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('weather:cache')->cron('0 */4 * * *');
+        $schedule->command('weather:cache')->cron('0 */12 * * *');
         $schedule->command('backup:clean')->daily()->at('01:00');
         $schedule->command('backup:run')->daily()->at('02:00');
         $schedule->command('sitemap:generate')->daily()->at('03:00');
