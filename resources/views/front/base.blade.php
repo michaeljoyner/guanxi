e846166a0f6dd06b99d2cd978dfef3217e56baa9
Michael Joyner <joyner.michael@gmail.com>
2021-05-20 16:14:10 +0800

    add google adsense script

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -17,6 +17,10 @@
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#ffc40d">
     <meta name="theme-color" content="#803EA0">
+
+    @production
+        <script data-ad-client="ca-pub-9642330358282954" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    @endproduction
 </head>
 <body class="pt-16 text-black font-sans antialiased @yield('bodyclass', 'scripted')">
 <!--[if lt IE 8]>
