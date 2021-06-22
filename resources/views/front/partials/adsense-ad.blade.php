ba2aee523fb211ed97b9c34a11c92c4e05ee8b48
Michael Joyner <joyner.michael@gmail.com>
2021-06-22 08:34:08 +0800

    wip

---
+++ resources/views/front/partials/adsense-ad.blade.php
@@ -0,0 +1,11 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Manualtest-multimedia -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9642330358282954"
+     data-ad-slot="4392284698"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
